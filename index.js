4fd791f567051dbcf269c2986e19bb33e97af13c
Frostyanimations126 <39423395+Frostyanimations126@users.noreply.github.com>
2021-05-07 08:53:52 -0400

    Fixed file paths

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 wdtEmojiBundle.init('.text');
-dtEmojiBundle.defaults.emojiSheets.apple = '/emoji/sheets/sheet_apple.png';
-wdtEmojiBundle.defaults.emojiSheets.google = '/emoji/sheets/sheet_google.png';
-wdtEmojiBundle.defaults.emojiSheets.twitter = '/emoji/sheets/sheet_twitter.png';
-wdtEmojiBundle.defaults.emojiSheets.emojione = '/emoji/sheets/sheet_emojione.png';
+wdtEmojiBundle.defaults.emojiSheets.apple = '/emoji/sheets/sheet_apple_64_indexed_128.png';
+wdtEmojiBundle.defaults.emojiSheets.google = '/emoji/sheets/sheet_google.png_64_indexed_128';
+wdtEmojiBundle.defaults.emojiSheets.twitter = '/emoji/sheets/sheet_twitter.png_64_indexed_128';
+wdtEmojiBundle.defaults.emojiSheets.emojione = '/emoji/sheets/sheet_emojione.png_64_indexed_128';
 wdtEmojiBundle.defaults.type = 'apple';
 wdtEmojiBundle.defaults.pickerColors = [
   'green', 'pink', 'yellow', 'blue', 'gray'
