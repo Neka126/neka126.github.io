66c5b660fe4c3a51c5fd2dcd7bf606c2da549449
Frostyanimations126 <39423395+Frostyanimations126@users.noreply.github.com>
2021-05-07 08:42:10 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+wdtEmojiBundle.init('.wdt-emoji-popup');
+dtEmojiBundle.defaults.emojiSheets.apple = '/emoji/sheets/sheet_apple.png';
+wdtEmojiBundle.defaults.emojiSheets.google = '/emoji/sheets/sheet_google.png';
+wdtEmojiBundle.defaults.emojiSheets.twitter = '/emoji/sheets/sheet_twitter.png';
+wdtEmojiBundle.defaults.emojiSheets.emojione = '/emoji/sheets/sheet_emojione.png';
+wdtEmojiBundle.defaults.type = 'apple';
+wdtEmojiBundle.defaults.pickerColors = [
+  'green', 'pink', 'yellow', 'blue', 'gray'
+];
