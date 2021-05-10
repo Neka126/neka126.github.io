0e6ecd83508ef801cd91c8a3a1ef95f818163358
Frostyanimations126 <39423395+Frostyanimations126@users.noreply.github.com>
2021-05-10 16:18:25 -0400

    Create NekaLinkz.js

---
+++ NekaLinkz.js
@@ -0,0 +1,6 @@
+neka.Linkz={
+  "LK-OAIEJDUAKEO":{
+    link:"https://cdpn.io/frostyanimations126/debug/ExWYYab/RBrOJRgvBwyM?doc=1DSTdLyxJCjfgDSh_3LD0YOOMq7OpvUxYwhPk5aNg4Bg/edit&type=gdraw",
+    LINK:"https://cdpn.io/frostyanimations126/debug/ExWYYab/RBrOJRgvBwyM?doc=1DSTdLyxJCjfgDSh_3LD0YOOMq7OpvUxYwhPk5aNg4Bg/edit&type=gdraw"
+  }
+}
